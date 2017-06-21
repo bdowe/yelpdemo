b138a55ab3f32a126446d5de76af066b223901cf
Brian Dowe <briandowe@gmail.com>
2017-06-20 19:25:37 -0700

    Add images with carrierwave, S3, and figaro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621001215) do
+ActiveRecord::Schema.define(version: 20170621014851) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170621001215) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
